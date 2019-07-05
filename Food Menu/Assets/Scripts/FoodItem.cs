473ac17a00dc40738f43217f327e48baa63d9e84
Matty <1016259@student.sae.edu.au>
2019-07-05 10:52:56 +0800

    Food Menu Added
    new project cause I'm so freaking lost

---
+++ Food Menu/Assets/Scripts/FoodItem.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoodItem : MonoBehaviour
+{
+    public enum FoodTypes
+    {
+        Eggs,
+        Bacon,
+        Fish,
+        Ham,
+        Toast,
+        Sausages,
+        Apples,
+        Cereal,
+        Porridge,
+        Soup,
+        Spaghetti
+    }
+
+    public FoodTypes myType;
+}
